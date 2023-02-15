0dc6f33172f247c3dbcd3aa88a8272fbe1f8c83c
axer665 <vellsevul@mail.ru>
2023-02-15 06:03:27 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,59 @@
+package org.example;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.opencsv.CSVReader;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
+        String fileName = "data.csv";
+
+        List<Employee> list = parseCSV(columnMapping, fileName);
+        String json = listToJson(list);
+        writeString(json);
+    }
+
+    private static void writeString(String json) {
+        try (FileWriter file = new FileWriter("data.json")) {
+            file.write(json);
+            file.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String listToJson(List<Employee> list) {
+        Type listType = new TypeToken<List<Employee>>() {}.getType();
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        String json = gson.toJson(list, listType);
+        return json;
+    }
+
+    private static List<Employee> parseCSV(String[] columnMapping, String fileName) {
+        try (CSVReader csvReader = new CSVReader(new FileReader(fileName))) {
+            ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<>();
+            strategy.setType(Employee.class);
+            strategy.setColumnMapping(columnMapping);
+            CsvToBean<Employee> csv = new CsvToBeanBuilder<Employee>(csvReader)
+                    .withMappingStrategy(strategy)
+                    .build();
+            List<Employee> staff = csv.parse();
+            return staff;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
